201cdfca4bf6abc04d8cad12fb3bc51f88ea5045
unknown <szymon.piatkowski27@gmail.com>
2020-08-14 16:24:18 +0200

    Listener for read more button

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // DOM
 const navButton = document.getElementById('nav-button');
+const aboutBtn = document.getElementById('about-btn');
 
 // Add class to hamburger icon
 function hamburgerIcon(){
     navButton.classList.toggle('header__change');
 }
 
+// Open subpage
+function openSubpage(){
+    location.href = "about.html";
+}
+
 
 // Events listeners
 navButton.addEventListener('click', hamburgerIcon);
+aboutBtn.addEventListener('click',openSubpage);
