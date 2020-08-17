232e4ffe80bf73dfa5ecf171c3db873dadb9f9d4
unknown <szymon.piatkowski27@gmail.com>
2020-08-17 19:58:00 +0200

    Script for scroll to the top button

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-// DOM
+// DOM elements
 const navButton = document.getElementById('nav-button');
 const aboutBtn = document.getElementById('about-btn');
+const scrollTop = document.querySelector("#scrollToTop");
+
 
 // Add class to hamburger icon
 function hamburgerIcon(){
@@ -12,7 +14,14 @@ function openSubpage(){
     location.href = "about.html";
 }
 
+//Scroll To the top button
+function scrollToTop(){
+    $('html,body').animate({scrollTop:0}, "slow");
+}
+
+
 
 // Events listeners
 navButton.addEventListener('click', hamburgerIcon);
 aboutBtn.addEventListener('click',openSubpage);
+scrollTop.addEventListener('click', scrollToTop);
